25345b180801a0341348f3818dbbeeba75fe494f
Yvonne <yvonne.seretse@outlook.com>
2023-03-21 15:49:02 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - Entry point
- *
+ * main - Entry point of the program
+ * Description: 'Print _putchar'
  * Return: Always 0 (Success)
  */
 int main(void)
